3e66036b5f4d92241209c0108954f8a6d32a1f0d
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2026-08-27 00:05:47 -0400

    added config service

--- services/api-gateway/src/main/java/com/acme/leads/gateway/GatewayApplication.java
+++ services/api-gateway/src/main/java/com/acme/leads/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.example.microservice.gateway;
+package com.acme.leads.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
